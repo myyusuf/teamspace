3988b260b786c240c40bfdff7e9d671ca4280ad5
Yusuf <myyusuf1911@gmail.com>
2014-07-17 17:49:25 +0700

    user-management first commit

---
+++ user-management/src/main/java/id/co/visitek/core/user_management/App.java
@@ -0,0 +1,13 @@
+package id.co.visitek.core.user_management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
